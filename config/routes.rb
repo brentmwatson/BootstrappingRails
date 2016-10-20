9a260f1ab1d34439fb9167b1897a50240e515b79
Brent Watson <brentmwatson@gmail.com>
2016-10-20 02:44:41 -0400

    just the code

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root '/' => 'welcome#index'
+  get '/welcome/(:id)' => 'welcome#show', as: :welcome
+  get '/client' => 'client#default', as: :client
+  get '/client/(:job)' => 'client#output'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
